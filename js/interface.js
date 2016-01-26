17af8db57bac557f01f5194c4c9277aa5381714a
CharlesAMoss <charlesamoss@yahoo.com>
2016-01-26 09:18:33 -0800

    add sizing element

--- js/interface.js
+++ js/interface.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
     console.log( "document loaded" );
     getViewport();
+
+    $( "div#container" ).click(function() {
+        console.log( "container loaded" );
+        // $(this).innerHeight = elHeight;
+        // $(this).innerWidth = elWidth;
+        // var elDiagonal = diagonal(elWidth, elHeight);
+        console.log(elDiagonal);
+    });
+
 });
 
 $( ".form__btn" ).click(function() {
