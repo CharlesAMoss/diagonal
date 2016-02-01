ce27d7d1343d81df800fe88516151a1d236f04b0
CharlesAMoss <charlesamoss@yahoo.com>
2016-02-01 08:51:01 -0800

    add some css

--- js/interface.js
+++ js/interface.js
@@ -1,7 +1,10 @@
 $( document ).ready(function() {
+    "use strict";
     console.log( "document loaded" );
     var foo = getViewport();
     console.log(foo);
+
+
 
     $( "div#container" ).click(function() {
         console.log( "container loaded" );
@@ -10,8 +13,16 @@ $( document ).ready(function() {
         var elDiagonal = diagonal(elWidth, elHeight);
         console.log(elDiagonal);
         console.log(getPercentage(elDiagonal,foo));
-
     });
+
+    // $( "div#container > img" ).click(function(elDiagonal) {
+    //     var imHeight = $('div#container > img').innerHeight();
+    //     var imWidth = $('div#container > img').innerWidth();
+    //     var imDiagonal = diagonal(imWidth, imHeight);
+    //     console.log(imDiagonal);
+    //     console.log(getPercentage(imDiagonal,elDiagonal));
+    // });
+
 });
 
 $( ".form__btn" ).click(function() {
